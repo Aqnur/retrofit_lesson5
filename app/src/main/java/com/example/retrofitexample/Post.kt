be054dcf5a5016220b024a6904ac9b068510a948
Aqnur <aknurr999@gmail.com>
2022-03-31 19:09:26 +0600

    fix

--- app/src/main/java/com/example/retrofitexample/Post.kt
+++ app/src/main/java/com/example/retrofitexample/Post.kt
@@ -3,7 +3,7 @@ package com.example.retrofitexample
 import com.google.gson.annotations.SerializedName
 
 data class Post(
-    @SerializedName("id") val postId: String?,
+    @SerializedName("id") val postId: Int?,
     val userId: String?,
     val title: String?,
     val body: String?
